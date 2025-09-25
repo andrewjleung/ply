50fdf7c3c9b6e6e342684b9f2ff0d636e3c9b697
Andrew Leung <andrewleung104@gmail.com>
2025-09-25 13:45:51 -0500

    Add ability to create applications without listing URL

--- src/command/to.rs
+++ src/command/to.rs
@@ -8,6 +8,7 @@ use crate::{
     command::Run,
     config,
     document::Filename,
+    job,
     scrape::{self, JobScraperKind, scrape},
 };
 use url::Url;
@@ -15,12 +16,24 @@ use url::Url;
 #[derive(Args)]
 pub struct To {
     /// The URL of the job listing
-    pub url: String,
+    pub url: Option<String>,
 
     /// The scraper to use, this will be inferred when the given URL scheme is 'https' and required
     /// if it is 'file'
-    #[arg(value_enum, long, short)]
+    #[arg(value_enum, long, short, requires("url"))]
     pub scraper: Option<JobScraperKind>,
+
+    /// The company for a new application, required if no listing URL is given
+    #[arg(long, conflicts_with("url"), required_unless_present("url"))]
+    pub company: Option<String>,
+
+    /// The job title for a new application, required if no listing URL is given
+    #[arg(long, conflicts_with("url"), required_unless_present("url"))]
+    pub title: Option<String>,
+
+    /// The job team for a new application
+    #[arg(long, conflicts_with("url"))]
+    pub team: Option<String>,
 
     // The job application cycle for this application
     #[arg(long, short)]
@@ -29,31 +42,52 @@ pub struct To {
 
 impl Run for To {
     fn run(&self, config: &config::PlyConfig) -> Result<()> {
-        let url = Url::parse(&self.url).context("failed to parse given URL")?;
-        let mut scraped = scrape(&url, self.scraper).context("failed to scrape URL")?;
-        let app = application::new(
-            scraped.job.to_owned(),
-            self.cycle
-                .to_owned()
-                .and_then(|c| c.is_empty().not().then_some(c))
-                .to_owned()
-                .or(config.default_cycle.to_owned()),
-        );
+        match &self.url {
+            Some(url) => {
+                let url = Url::parse(url).context("failed to parse given URL")?;
+                let mut scraped = scrape(&url, self.scraper).context("failed to scrape URL")?;
+                let app = application::new(
+                    scraped.job.to_owned(),
+                    self.cycle
+                        .to_owned()
+                        .and_then(|c| c.is_empty().not().then_some(c))
+                        .to_owned()
+                        .or(config.default_cycle.to_owned()),
+                );
 
-        // TODO: handle repeat applications to the same listing
-        if let Ok(filename) = scraped.job.filename() {
-            scrape::snapshot_content(
-                &mut scraped.content,
-                &config.data_dir.join("listings"),
-                &filename,
-            )
-            .context("failed to snapshot content")?;
+                // TODO: handle repeat applications to the same listing
+                if let Ok(filename) = scraped.job.filename() {
+                    scrape::snapshot_content(
+                        &mut scraped.content,
+                        &config.data_dir.join("listings"),
+                        &filename,
+                    )
+                    .context("failed to snapshot content")?;
+                }
+
+                app.write_new_document(config)?;
+
+                println!("application created at {}", app.filename());
+
+                Ok(())
+            }
+            None => {
+                let job = job::Job {
+                    company: self.company.to_owned().unwrap(),
+                    title: self.title.to_owned().unwrap(),
+                    team: self.team.to_owned(),
+                    listing_url: None,
+                    salary_range: None,
+                };
+
+                let app = application::new(job, self.cycle.clone());
+
+                app.write_new_document(config)?;
+
+                println!("application created at {}", app.filename());
+
+                Ok(())
+            }
         }
-
-        app.write_new_document(config)?;
-
-        println!("application created at {}", app.filename());
-
-        Ok(())
     }
 }
